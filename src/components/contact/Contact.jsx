1373257ff0809279317c78a8eb88abeaada73183
fbd3887 <sky.blue.1991519@gmail.com>
2021-10-29 14:18:24 +0800

    remove node modules

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -102,7 +102,7 @@ export default function Contact(){
                         <div className="row">
                             <div className="col-md-7">
                                 <div className="row">
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3615.069603940545!2d121.54702211544704!3d25.031711844536193!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442abcd6f6d08a7%3A0x77dd93bfa48ba4ae!2zMTA2ODEsIFRhaXdhbiwgVGFpcGVpIENpdHksIERh4oCZYW4gRGlzdHJpY3QsIFNlY3Rpb24gMiwgRHVuaHVhIFMgUmQsIDM56JmfMTLmqJM!5e0!3m2!1sen!2sin!4v1635416501552!5m2!1sen!2sin" width="100%" height="450" allowfullscreen="" loading="lazy"></iframe>
+                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3615.069603940545!2d121.54702211544704!3d25.031711844536193!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3442abcd6f6d08a7%3A0x77dd93bfa48ba4ae!2zMTA2ODEsIFRhaXdhbiwgVGFpcGVpIENpdHksIERh4oCZYW4gRGlzdHJpY3QsIFNlY3Rpb24gMiwgRHVuaHVhIFMgUmQsIDM56JmfMTLmqJM!5e0!3m2!1sen!2sin!4v1635416501552!5m2!1sen!2sin" width="100%" height="450" allowfullscreen="" loading="lazy" title="This is a medical contact" ></iframe>
                                 </div>
                             </div>
                             <div className="col-md-5">
