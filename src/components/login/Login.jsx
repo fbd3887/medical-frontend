8ed5c920aed3c100bcb722693d59b4f56d5546cc
jaamasad1 <jaamasad1@gmail.com>
2021-11-11 00:21:21 +0500

    login page responsivenes

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -3,43 +3,41 @@ import './login.scss';
 import logo2 from '../../images/logo2.png';
 import { Link } from "react-router-dom";
 
-export default function Login(){
-    return(
+export default function Login() {
+    return (
         <div className="loginBackground">
             <div className="row min-vh-100 ">
-                <div className="col-md-5 my-auto leftlogin">
+                <div className="col-md-6 my-auto mb-4 mb-md-auto leftlogin">
                     <h1 className="text-center m-5">The best physiological maker: AMH</h1>
-                    <h3 className="text-center  m-5">Not for fertility pr other people,<br/> start understaning your body with AMH</h3>
-                    <h3 className="">Do not have an account yet? <Link to="/register">Click here to register</Link></h3>
+                    <h3 className="text-center  m-5">Not for fertility pr other people,<br /> start understaning your body with AMH</h3>
+                    <h3 className="d-flex align-items-center flex-column">Do not have an account yet? <Link to="/register">Click here to register</Link></h3>
                 </div>
-                <div className="col-md-7 my-auto text-center">
-                    <div className="loginFormDiv">
+                <div className="col-md-6 my-auto text-center">
+                    <div className="loginFormDiv px-3 col-sm-7 col-sm-10">
                         <img src={logo2} alt="" />
                         <div className="loginHeading text-left">
                             <h1>Log In</h1>
                         </div>
                         <div className="loginFeild">
                             <div className="innerFieldDiv">
-                                <label>EMAIL</label><br/>
-                                <input type="email"/>
+                                <label>EMAIL</label><br />
+                                <input type="email" />
                             </div>
                         </div>
                         <div className="loginFeild">
                             <div className="innerFieldDiv">
                                 <label>PASSWORD <span className="forgetLogin">FORGET?</span></label>
-                                <input type="password"/>
+                                <input type="password" />
                             </div>
                         </div>
-                        <Link to="/accountinformation"><button className="btn">
-                            Login
-                        </button></Link>
+                        <Link to="/accountinformation">
+                            <button className="btn">
+                                Login
+                            </button>
+                        </Link>
                     </div>
-                    
                 </div>
             </div>
-            
-
         </div>
-        
     )
 }
